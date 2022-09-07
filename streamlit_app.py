3c3f41e5d7f88b7e4942168cf17778810cbaaf5b
Stammers <Matthew.Stammers@uhs.nhs.uk>
2022-09-07 18:51:02 +0100

    Starting Point Selector Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,18 @@ st.title('Over 78 Week Wait Goal Gap Model')
 
 # Add Variables to the Smoking Cessation SD Class object
 
+st.subheader('Enter the Size of the Backlog')
+start_point = st.number_input(label='Backlog Size', min_value=None, max_value=None, value=2500)
 st.subheader('Slide the Slider to Vary The Adjustment Time')
 adjustment_time = st.slider("Adjustment Time", 1, 12, 1)
 
 # Run the Model
-values = model.run(params={'Adjustment': adjustment_time})
+
+values = model.run(initial_condition=(0,{'> 78 weeks': start_point}), params={'Adjustment': adjustment_time})
 
 # Export the Simulation Results
 
 df_waiters = values[['> 78 weeks','Closed Long- Wait Pathways']]
 st.subheader('Effects of Increasing Effort to Close Long Waiter Pathways')
 st.line_chart(df_waiters)
+print(values)

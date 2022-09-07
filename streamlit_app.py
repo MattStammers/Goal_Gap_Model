167e6a112deeca693eb58b767d655d81ee20921d
Stammers <Matthew.Stammers@uhs.nhs.uk>
2022-09-07 17:42:37 +0100

    Goal Gap Model Complete

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,25 @@
 # Firstly import the packages
 
 import pysd
-from IPython.display import Image
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-model = pysd.read_xmile('New_Model.stmx')
+model = pysd.read_xmile('Revised_Goal_Gap.stmx')
 
 # Name the app
 
-st.title('Smoking Cessation SD Model Demo')
+st.title('Over 78 Week Wait Goal Gap Model')
 
 # Add Variables to the Smoking Cessation SD Class object
 
-st.subheader('Slide the Slider to Vary Smoking Cessation Re-Investment Levels')
-re_investment = st.slider("Proportion of Savings Spent on Cessation", 1, 100, 1)
+st.subheader('Slide the Slider to Vary The Adjustment Time')
+adjustment_time = st.slider("Adjustment Time", 1, 12, 1)
 
 # Run the Model
-values = model.run(params={'Percentage of savings spent on cessation': re_investment})
+values = model.run(params={'Adjustment': adjustment_time})
 
 # Export the Simulation Results
 
-df_smoking = values[['Current smokers', 'Ex smokers', 'Lapsed ex smokers']]
-
-st.subheader('Effects of Re-Investment on Smoking Levels')
-st.line_chart(df_smoking)
+df_waiters = values[['> 78 weeks','Closed Long- Wait Pathways']]
+st.subheader('Effects of Increasing Effort to Close Long Waiter Pathways')
+st.line_chart(df_waiters)
